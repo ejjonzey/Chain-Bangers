7d1d6e8fd80863fcab944e4395bae19373675476
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-11 15:23:28 -0400

    seeds for holes.

--- db/seeds.js
+++ db/seeds.js
@@ -30,7 +30,7 @@ const EastRoswell = new Course({
     description: "A balanced mix of wide open field holes with scattered trees and short-moderate length fairways in the woods. Lots of in and out of the woods play. Can play from really short to average length depending on pin position and tee used. Pond in play on 17",
 })
 
-const admin = new User({
+const Admin = new User({
     first_name: 'Eric',
     last_name: 'Jonelunas',
     username: 'Ejjonzey',
@@ -38,10 +38,51 @@ const admin = new User({
     admin: True
 })
 
+const Hole = new Hole1({
+    number: '1',
+    distance: '350',
+    par: '3',
+    pinlocation: 'A'
+})
+
+const Hole = new Hole2({
+    number: '2',
+    distance: '350',
+    par: '3',
+    pinlocation: 'A'
+})
+
+const Hole = new Hole3({
+    number: '3',
+    distance: '350',
+    par: '3',
+    pinlocation: 'A'
+})
+
+const Hole = new Hole4({
+    number: '4',
+    distance: '350',
+    par: '3',
+    pinlocation: 'A'
+})
+
+const Hole = new Hole5({
+    number: '5',
+    distance: '350',
+    par: '3',
+    pinlocation: 'A'
+})
+
 Alexander.save().then(()=> console.log("Course Saved!"));
 Redan.save().then(()=> console.log("Course Saved!"));
 EastRoswell.save().then(()=> console.log("Course Saved!"));
 
-admin.save().then(()=> console.log("Admin saved"));
+Hole1.save().then(()=> console.log('Hole1 saved!'));
+Hole2.save().then(()=> console.log('Hole2 saved!'));
+Hole3.save().then(()=> console.log('Hole3 saved!'));
+Hole4.save().then(()=> console.log('Hole4 saved!'));
+Hole5.save().then(()=> console.log('Hole5 saved!'));
+
+Admin.save().then(()=> console.log("Admin saved"));
 
 mongoose.connection.close();
