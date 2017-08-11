dbafed9cbfde58bbc8e0636034c9cbd8de148825
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-11 19:25:29 -0400

    Added edit hole component.

---
+++ client/src/components/EditHole.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditHole extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default EditHole;
