476d5ac0d032da485f74cd9163998854084d0da7
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-11 13:32:45 -0400

    first step heroku

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.get('/', (req,res) => {
   res.send('Hello world!')
 })
 
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
