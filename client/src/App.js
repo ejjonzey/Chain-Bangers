e03af622b697603a2404a4df08af7bb57d76fa4a
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-11 19:24:45 -0400

    add course route works.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from './components/Home';
 import Course from './components/Course';
 import AddCourse from './components/AddCourse';
+
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home/>
-        <Course/>
-        <AddCourse/>
-      </div>
+      <Router>
+        <div>
+          <div>
+            <Link to='/'>Home</Link>
+            <Link to='/AddCourse'>Add Course</Link>
+          </div>
+          <div>
+            <Route exact path="/" component={Home}/>
+            <Route  path="/AddCourse" component={AddCourse}/>
+          </div>
+        </div>
+        </Router>
     );
   }
 }
