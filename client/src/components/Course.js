005752e4e87add0811aa0048495ee224065a7c91
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-12 16:06:14 -0400

    i dont know why the axios isnt working.

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import {Link}from 'react-router-dom';
+import axios from 'axios';
 
 
 class Course extends Component {
-    render() {
+    constructor(){
+        super();
+        this.state={
+            user: "",
+            holes: ""
+        }
+    }
+
+    componentWillMount(){
+        axios.get('localhost:3001/api/user').then(res=>{
+            console.log(res.data);
+            this.setState({courses: res.data})
+        });
+    }
+    render(){
         return (
             <div>
                 <div>
-                <h1>Hello from Course</h1>
+                <h1>These are your courses</h1>
                 <Link to='/AddCourse'>Add Course</Link>
                 </div>
+                {this.state.courses.map((course, i)=>(
+                    <div key={i}>
+                        <Link to={`/course/${course._id}`}>
+                        {course.user}'s Course
+                        </Link>
+                        </div>
+                ))}
             </div>
         );
     }
