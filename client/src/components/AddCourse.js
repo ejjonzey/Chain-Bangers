d7d588f2969778c5e1f5ad59d6386d123d9b61b1
Ejjonzey <ejjonzey1982@gmail.com>
2017-09-25 13:46:41 -0400

    validations required

--- client/src/components/AddCourse.js
+++ client/src/components/AddCourse.js
@@ -48,20 +48,20 @@ class AddCourse extends Component {
                    <h1>Add A Course</h1>
                <form onSubmit={this._handleSubmit}>
                <label>
-              Name:
-              <input type="text" name="name" onChange={this._handleChange} value={this.state.course.name}/>
+               <label htmlFor="name">Name: </label>
+               <input type="text" name="name" onChange={this._handleChange} value={this.state.course.name} required/>
               <br/>
-              Location:
-              <input type="text" name="location" onChange={this._handleChange} value={this.state.course.location}/>
+              <label htmlFor="location">Location: </label>
+              <input type="text" name="location" onChange={this._handleChange} value={this.state.course.location} required/>
               <br/>
-              Holes:
-              <input type="text" name="holes" onChange={this._handleChange} value={this.state.course.holes}/>
+              <label htmlFor="holes">Holes: </label>
+              <input type="text" name="holes" onChange={this._handleChange} value={this.state.course.holes} required/>
               <br/>
-              Image:
+              <label htmlFor="image">image: </label>
               <input type="text" name="image" onChange={this._handleChange} value={this.state.course.image}/>
               <br/>
-              description:
-              <input type="text" name="description"onChange={this._handleChange} value={this.state.course.description} />
+              <label htmlFor="description">Description: </label>
+              <input type="text" name="description"onChange={this._handleChange} value={this.state.course.description} required/>
               <button type="submit" value="Submit">Submit</button>
                </label>
               </form>    
